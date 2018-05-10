1b4482bf0b845c9176997c53ed61e5fde2dd855a
Eddy Drew <eddy@xcap.ca>
2018-05-10 12:53:58 -0400

    Added the script

---
+++ main.js
@@ -0,0 +1,21 @@
+//FizzBuzz exercise:
+//Count from 1 to 20, if the number is divisible by 3, print 'Fizz', if it's divisible by 5, print'Buzz', if divisible by both, print 'FizzBuzz'
+
+for (i = 1; i <100; i++) {
+
+  if (i % 3 === 0 && i % 5 === 0) {
+      console.log("FizzBuzz");
+  }
+
+  else if (i % 3 === 0) {
+   console.log("Fizz");
+   }
+
+  else if (i % 5 === 0) {
+   console.log("Buzz");
+  }
+
+  else {
+      console.log(i);
+      }
+};
