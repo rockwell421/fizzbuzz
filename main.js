b31527f5140426785918d2fe25b11055918e6932
Eddy Drew <eddy@xcap.ca>
2018-05-10 12:59:14 -0400

    updated

--- main.js
+++ main.js
@@ -1,21 +1,25 @@
 //FizzBuzz exercise:
-//Count from 1 to 20, if the number is divisible by 3, print 'Fizz', if it's divisible by 5, print'Buzz', if divisible by both, print 'FizzBuzz'
+//Count from 1 to 100, if the number is divisible by 3, print 'Fizz', if it's divisible by 5, print'Buzz', if divisible by both, print 'FizzBuzz'
 
-for (i = 1; i <100; i++) {
+function fizzBuzz() {
+  for (i = 1; i <= 100; i++) {
 
-  if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FizzBuzz");
-  }
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    }
 
-  else if (i % 3 === 0) {
-   console.log("Fizz");
-   }
+    else if (i % 3 === 0) {
+     console.log("Fizz");
+     }
 
-  else if (i % 5 === 0) {
-   console.log("Buzz");
-  }
+    else if (i % 5 === 0) {
+     console.log("Buzz");
+    }
 
-  else {
-      console.log(i);
-      }
-};
+    else {
+        console.log(i);
+        }
+    }
+}
+
+//call fizzBuzz();
